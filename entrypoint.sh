48b0624596358206a968c445641e9879fef446ad
YamenW <yamen.z.wazwaz@gmail.com>
2024-06-18 17:37:20 +0300

    fix files being sourced too early

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
-source ~/.config/yamens/functions.sh
-source ~/.config/yamens/nnn-config.sh
 eval "$(atuin init bash --disable-up-arrow --disable-ctrl-r)"
 export PATH=$PATH:/home/yamen/.exec
 eval "$(oh-my-posh init bash --config ~/atomic.omp.json)"
 eval "$(zoxide init bash)"
 export _ZO_ECHO=1
+
+source ~/.config/yamens/functions.sh
+source ~/.config/yamens/nnn-config.sh

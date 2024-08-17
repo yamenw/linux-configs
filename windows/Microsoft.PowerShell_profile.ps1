dab51f5ed243c66f99ee9e6efb40678b73d5ac67
yamenw <yamentaad@gmail.com>
2024-08-17 14:19:31 +0300

    add cssh function to pwsh

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,6 @@
-oh-my-posh.exe init pwsh --config "$MY_POSH_THEME" | Invoke-Expression
+# IMPORTANT: OMP will use the default theme if the path is incorrect
+# Without printing any errors
+oh-my-posh.exe init pwsh --config "$OMP_THEME_PATH" | Invoke-Expression
 
 if ($host.Name -eq 'ConsoleHost') {
     Import-Module PSReadLine
@@ -28,4 +30,20 @@ ProxyCommand bash -c "ncat $(powershell -File ~/.ssh/get-laptop-ip.ps1) %p"
 #>
 function laptopip {
     Write-Output $(Get-NetNeighbor -AddressFamily IPv4 | Where-Object { $_.LinkLayerAddress -eq $LAPTOP_MAC_ADDRESS } | Select-Object -ExpandProperty IPAddress)
+}
+
+
+function get-host-info([string]$HOSTNAME) {
+    & "C:\Program Files\Git\usr\bin\ssh.exe" -TG $HOSTNAME | Select-String -Pattern "^user ", "^hostname", "^identityfile ", "^port " | ForEach-Object { $_.Line }
+}
+
+function cssh {
+    $ALL_HOSTS = $(Get-Content ~\.ssh\config | Select-String -Pattern "^Host ")
+    $SELECTED_HOST=$(Write-Output $ALL_HOSTS | ForEach-Object { ($_ -split '\s+')[1] } | fzf --multi)
+
+    if (-not $? -or -not $SELECTED_HOST) {
+        return
+    }
+
+    & 'C:/Program Files/Git/usr/bin/ssh.exe' $SELECTED_HOST
 }
